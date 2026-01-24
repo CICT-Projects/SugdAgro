fa3de0af730a4a09abdeb11649559fa2b054e931
Habibulloh2102 <hafizulloachilov@gmail.com>
2026-01-24 17:35:25 +0300

    Исправить пространство имен в классе BaseEntity на SugdAgro.Models

--- server/SugdAgro/Models/BaseEntity.cs
+++ server/SugdAgro/Models/BaseEntity.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TZ2401.Models
+namespace SugdAgro.Models
 {
     public abstract class BaseEntity
     {
+    
     public int Id { get; set; }
+    
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    
     public DateTime? UpdatedAt { get; set; }
+    
     public bool IsPublished { get; set; }
     }
 }
