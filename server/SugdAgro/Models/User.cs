8da5feacb5d540adabbf120e61660146d25492c5
realhexpro-dev <realhexpro@gmail.com>
2026-01-25 12:12:44 +0500

    auth

--- server/SugdAgro/Models/User.cs
+++ server/SugdAgro/Models/User.cs
@@ -2,7 +2,7 @@ namespace SugdAgro.API.Models;
 
 public class User
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
     public string Email { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
 }
